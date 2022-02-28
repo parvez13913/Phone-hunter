d91d523c38404291e7d5130456206de78978e92f
parvez13913 <parvezz13913@gmail.com>
2022-02-28 23:17:49 +0600

    showing display data,adnd details button complite

--- js/index.js
+++ js/index.js
@@ -1,4 +1,4 @@
-const loadPhone = () => {
+const searchPhone = () => {
     const searchField = document.getElementById('search-field');
     const searchText = searchField.value;
     const url = `https://openapi.programming-hero.com/api/phones?search=${searchText}`;
@@ -10,6 +10,23 @@ const loadPhone = () => {
 }
 
 // showing disply data
-const showDisply = phone => {
-    console.log(phone);
+const showDisply = phones => {
+    const searchResult = document.getElementById('search-result');
+    phones.forEach(phone =>{
+        console.log(phone);
+        const displyDiv = document.createElement('div');
+        displyDiv.classList.add('col');
+        displyDiv.innerHTML = `
+            <div class="card h-100 w-75 mx-auto p-3 border-0 shadow-lg rounded">
+                   <img src="${phone.image}" class="card-img-top" alt="...">
+                <div class="card-body">
+                   <h6 class="card-title">${phone.phone_name}</h6>
+                   <h5 class="card-title">${phone.brand}</h5>
+                   <button class = "btn bg-color text-white"> Details </button>
+                </div>
+            </div>
+        `;
+        searchResult.appendChild(displyDiv);
+    });
+
 }
